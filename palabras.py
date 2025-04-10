1d8247629fa59eace25b1ad95d68278b8191a7a3
Erik-Ivar-03 <148734874+Erik-Ivar-03@users.noreply.github.com>
2025-04-10 16:54:50 -0500

    Update palabras.py

--- palabras.py
+++ palabras.py
@@ -3,5 +3,6 @@
 import random
 
 def obtener_palabra():
+    ##palabras con las que se jugaran en el juego del ahorcado
     palabras = ["python", "desarrollador", "programacion", "computadora", "ahorcado"]
     return random.choice(palabras)
